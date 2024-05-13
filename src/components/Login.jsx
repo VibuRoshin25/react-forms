d061b6ba74d30da14b869f2f64d27c030c4b4bbc
Vibu Roshin E V <viburoshin57@gmail.com>
2024-05-13 11:06:21 +0530

    modified:   src/App.jsx 	modified:   src/components/Login.jsx 	new file:   src/components/Signup.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,9 @@ export default function Login() {
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
   }
 
   return (
@@ -15,12 +18,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
